e23a30a425b9240a6c9efe732d8001b458571ac8
lewis <lewdon0001@gmail.com>
2026-02-04 19:35:36 +0000

    Script fixed i think

--- lyrwal.sh
+++ lyrwal.sh
@@ -15,27 +15,11 @@ fi
 mkdir -p $HOME/Pictures
 mkdir -p $HOME/Pictures/lyrwal
 
-# Generate configs if not there
-mkdir -p $HOME/.config/lyrwal
 set_wallpaper_path="$HOME/.config/lyrwal/set-wallpaper.sh"
 config_toml_path="$HOME/.config/lyrwal/config.toml"
 
-if [ ! -e "$set_wallpaper_path" ]; then
-    echo "Creating $set_wallpaper_path..."
-    cat <<EOL > "$set_wallpaper_path"
-#!/usr/bin/env bash
-
-WALLPAPER_DIR=\$(lyrwal get wallpaper_dir)
-echo 'You have not created a script to set your wallpaper!'
-echo 'Please edit $HOME/.config/lyrwal/set-wallpaper.sh to properly set the wallpaper based on WALLPAPER_DIR.'
-EOL
-    chmod +x "$set_wallpaper_path"
-    echo "$set_wallpaper_path has been created."
-fi
-
-if [ ! -e "$config_toml_path" ]; then
-    echo "Creating $config_toml_path..."
-    cat <<EOL > "$config_toml_path"
+write_cfg() {
+  cat <<EOL > "$config_toml_path"
 [genius]
 api_key = "YOUR_API_KEY"
 
@@ -59,11 +43,36 @@ char_align = "left"
 wallpaper_dir = "$HOME/Pictures/lyrwal/wallpaper.png"
 command = "$HOME/.config/lyrwal/set-wallpaper.sh"
 EOL
-    echo "$config_toml_path has been created."
-    echo "You may need to get an API key for genius for $config_toml_path, and edit $set_wallpaper_path to correctly set your wallpaper."
-fi
+}
+
+write_set_wallpaper() {
+  echo "Creating $set_wallpaper_path..."
+  cat <<EOL > "$set_wallpaper_path"
+#!/usr/bin/env bash
+
+WALLPAPER_DIR=\$(lyrwal get wallpaper_dir)
+echo 'You have not created a script to set your wallpaper!'
+echo 'Please edit $HOME/.config/lyrwal/set-wallpaper.sh to properly set the wallpaper based on WALLPAPER_DIR.'
+EOL
+}
 
 if [ "$cmd" = "update" ]; then
+  # Generate configs if not there
+  mkdir -p $HOME/.config/lyrwal
+
+  if [ ! -e "$set_wallpaper_path" ]; then
+      write_set_wallpaper
+      chmod +x "$set_wallpaper_path"
+      echo "$set_wallpaper_path has been created."
+  fi
+
+  if [ ! -e "$config_toml_path" ]; then
+      echo "Creating $config_toml_path..."
+      write_config
+      echo "$config_toml_path has been created."
+      echo "You may need to get an API key for genius for $config_toml_path, and edit $set_wallpaper_path to correctly set your wallpaper."
+  fi
+
     cd $HOME/.lyrwal/py
     ./.venv/bin/python ./main.py update
 
